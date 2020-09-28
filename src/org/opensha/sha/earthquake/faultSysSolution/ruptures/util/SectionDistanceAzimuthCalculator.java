fb2c792d420b205568a10eb57f9b62dc4c270815
volkertj <o.jurgens@gns.cri.nz>
2020-09-28 14:52:07 +1300

    fix  bug
    (cherry picked from commit 18383924a87ca5edfe0ac45c501a0f34fdf50143)

--- src/org/opensha/sha/earthquake/faultSysSolution/ruptures/util/SectionDistanceAzimuthCalculator.java
+++ src/org/opensha/sha/earthquake/faultSysSolution/ruptures/util/SectionDistanceAzimuthCalculator.java
@@ -123,7 +123,7 @@ public class SectionDistanceAzimuthCalculator {
 	}
 	
 	public ConcurrentMap<IDPairing, Double> getCachedAzimuths() {
-		return distCache.asMap();
+		return azCache.asMap();
 	}
 	
 	public void writeCacheFile(File cacheFile) throws IOException {
